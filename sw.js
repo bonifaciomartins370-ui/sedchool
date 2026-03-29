52b654ade2d5760b1bedc6ad1a461739b3e07abc
bonifaciomartins370-ui <bonifaciomartins370@gmail.com>
2026-03-29 01:03:37 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "sedchool-cache-v1";
+const urlsToCache = [
+  "/sedchool/",
+  "/sedchool/index.html",
+  "/sedchool/style.css",
+  "/sedchool/script.js"
+];
+
+// Instalar o service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativar (limpar cache antigo)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar requisições (offline support)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
